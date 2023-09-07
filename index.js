8daf923e51eb95fccf8676de9e40d85155b22024
giangpham-cfg <giang.pham1610@gmail.com>
2023-09-07 20:51:41 +0200

    Update try and catch

--- index.js
+++ index.js
@@ -13,158 +13,146 @@ app.get("/", (req, res) => {
 
 //get request
 app.get("/messages", async (req, res) => {
-  const nestMessages = (messages, parentId = null) => {
-    const nestedMessages = [];
+  try {
+    const nestMessages = (messages, parentId = null) => {
+      const nestedMessages = [];
 
-    for (const message of messages) {
-      if (message.parentId === parentId) {
-        // Recursively nest child messages
-        const children = nestMessages(messages, message.id);
-        if (children.length > 0) {
-          message.children = children;
+      for (const message of messages) {
+        if (message.parentId === parentId) {
+          // Recursively nest child messages
+          const children = nestMessages(messages, message.id);
+          if (children.length > 0) {
+            message.children = children;
+          }
+          nestedMessages.push(message);
         }
-        nestedMessages.push(message);
       }
-    }
-    return nestedMessages;
-  };
+      return nestedMessages;
+    };
 
-  const messages = await prisma.message.findMany({
-    include: {
-      children: true,
-    },
-  });
-  // Use the recursive function to nest the messages properly
-  const nestedMessages = nestMessages(messages);
-  res.send({ success: true, messages: nestedMessages });
+    const messages = await prisma.message.findMany({
+      include: {
+        children: true,
+      },
+    });
+    // Use the recursive function to nest the messages properly
+    const nestedMessages = nestMessages(messages);
+    res.send({ success: true, messages: nestedMessages });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 //delete request
 app.delete("/messages/:messageId", async (req, res) => {
-  const { messageId } = req.params;
-  const message = await prisma.message.delete({
-    where: {
-      id: messageId,
-    },
-  });
-  res.send({ success: true, message });
+  try {
+    const { messageId } = req.params;
+    if (!messageId)
+      return res.send({
+        success: false,
+        error: "The message not found.",
+      });
+    const message = await prisma.message.delete({
+      where: {
+        id: messageId,
+      },
+    });
+    res.send({ success: true, message });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 //post request
 app.post("/messages", async (req, res) => {
-  // const { text, parentId } = req.body;
+  try {
+    const { text, parentId } = req.body;
 
-  // if (!text)
-  //   return res.send({
-  //     success: false,
-  //     error: "Text must be provided to create a message!",
-  //   });
+    if (!text)
+      return res.send({
+        success: false,
+        error: "Text must be provided to create a message!",
+      });
 
-  // let message;
+    // Check if parentId is provided
+    if (parentId) {
+      // Create a child message
+      const parentMessage = await prisma.message.findUnique({
+        where: { id: parentId },
+      });
 
-  // if (!parentId) {
-  //   message = await prisma.message.create({
-  //     data: {
-  //       text,
-  //       parentId,
-  //     },
-  //   });
-  // } else {
-  //   message = await prisma.children.create({
-  //     data: {
-  //       text,
-  //       message: {
-  //         connect: {
-  //           id: parentId,
-  //         },
-  //       },
-  //     },
-  //   });
-  // }
+      if (!parentMessage) {
+        return res.send({ error: "The message not found." });
+      }
 
-  const { text, parentId } = req.body;
+      const childMessage = await prisma.message.create({
+        data: {
+          text,
+          parentId,
+        },
+      });
 
-  if (!text)
-    return res.send({
-      success: false,
-      error: "Text must be provided to create a message!",
-    });
+      return res.send({ success: true, message: childMessage });
+    } else {
+      // Create a top-level message
+      const message = await prisma.message.create({
+        data: {
+          text,
+        },
+      });
 
-  // Check if parentId is provided
-  if (parentId) {
-    // Create a child message
-    const parentMessage = await prisma.message.findUnique({
-      where: { id: parentId },
-    });
-
-    if (!parentMessage) {
-      return res.send({ error: "The message not found." });
+      return res.send({ success: true, message });
     }
-
-    const childMessage = await prisma.message.create({
-      data: {
-        text,
-        parentId,
-      },
+  } catch (error) {
+    console.log(error);
+    res.send({
+      success: false,
+      error: error.message,
     });
-
-    return res.send({ success: true, message: childMessage });
-  } else {
-    // Create a top-level message
-    const message = await prisma.message.create({
-      data: {
-        text,
-      },
-    });
-
-    return res.send({ success: true, message });
   }
 });
-// const { text, parentId } = req.body;
-
-//   if (!text)
-//     return res.send({
-//       success: false,
-//       error: "Text must be provided to create a message!",
-//     });
-
-//   let messageToCreate = {
-//     text,
-//   };
-
-//   if (parentId) {
-//     // If parentId is provided, we create a child message
-//     messageToCreate = {
-//       ...messageToCreate,
-//       parentId,
-//     };
-//   }
-
-//   const createdMessage = await prisma.message.create({
-//     data: messageToCreate,
-//   });
-//   res.send({ success: true, message: createdMessage });
-// });
 
 //put request
 app.put("/messages/:messageId", async (req, res) => {
-  const { messageId } = req.params;
-  const { text, likes } = req.body;
-  if (!text && !likes)
-    return res.send({
-      success: false,
-      error: "Should provide text or likes to update a message!",
+  try {
+    const { messageId } = req.params;
+    const { text, likes } = req.body;
+
+    if (!messageId)
+      return res.send({
+        success: false,
+        error: "The message not found.",
+      });
+    if (!text && !likes)
+      return res.send({
+        success: false,
+        error: "Should provide text or likes to update a message!",
+      });
+    const message = await prisma.message.update({
+      where: {
+        id: messageId,
+      },
+      data: {
+        text,
+        likes,
+      },
     });
-  const message = await prisma.message.update({
-    where: {
-      id: messageId,
-    },
-    data: {
-      text,
-      likes,
-    },
-  });
-  res.send({ success: true, message });
+    res.send({ success: true, message });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 //handle error when client uses wrong route
